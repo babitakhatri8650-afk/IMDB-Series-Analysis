650eeba921840995c10485c83c89549e2e0ad31e
Babita Khatri <babitakhatri8650@gmail.com>
2026-03-03 10:29:23 -0300

    Update schema.sql
    Signed-off-by: Babita Khatri <babitakhatri8650@gmail.com>

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE imdb_project;
+USE imdb_project;
 
+CREATE TABLE series (
+    email VARCHAR(100),
+    series_title VARCHAR(200),
+    released_year INT,
+    certificate VARCHAR(20),
+    runtime VARCHAR(20),
+    imdb_rating DECIMAL(3,1),
+    overview TEXT
+);
